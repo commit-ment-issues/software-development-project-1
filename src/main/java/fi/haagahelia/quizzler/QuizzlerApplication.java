c595d06539b91312efac90b4399c6ef57b907dad
Noora Laakso <nooralaakso@Noora-MacBook-Air.local>
2025-04-01 15:20:16 +0300

    init

---
+++ src/main/java/fi/haagahelia/quizzler/QuizzlerApplication.java
@@ -0,0 +1,13 @@
+package fi.haagahelia.quizzler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizzlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizzlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fi/haagahelia/quizzler/QuizzlerApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.haagahelia.quizzler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizzlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
