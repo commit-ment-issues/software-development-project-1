4e3cc5b426c06374d87b7ec57f7c5bafb1795ccb
robbiewinter <robbie.winter@myy.haaga-helia.fi>
2025-04-03 15:54:47 +0300

    Modified Question entity

--- src/main/java/domain/Question.java
+++ src/main/java/domain/Question.java
@@ -1,10 +1,16 @@
 package domain;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+
     private String question, difficulty;
 
     public Question(String question, String difficulty) {
