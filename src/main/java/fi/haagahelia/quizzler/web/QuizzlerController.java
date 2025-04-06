7f2bad1cddab1f2b4e2d363be3d68af3e8fa82b9
Kaislakudjoi <159912977+Kaislakudjoi@users.noreply.github.com>
2025-04-06 15:22:51 +0300

    add quizlist functionality

--- src/main/java/fi/haagahelia/quizzler/web/QuizzlerController.java
+++ src/main/java/fi/haagahelia/quizzler/web/QuizzlerController.java
@@ -1,5 +1,21 @@
 package fi.haagahelia.quizzler.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import fi.haagahelia.quizzler.domain.QuizRepository;
+
+@Controller
 public class QuizzlerController {
+
+    @Autowired QuizRepository quizRepository;
+
+    @RequestMapping(value = {"/quizlist"})
+    public String quizList(Model model) {
+        model.addAttribute("quizzes", quizRepository.findAll());
+        return "quizlist";
+    }
 
 }
