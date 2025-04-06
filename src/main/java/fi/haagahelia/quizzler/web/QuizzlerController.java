dc447516ed930c2cac1134b08de7824c829c25ff
nooralaakso <bhm543@myy.haaga-helia.fi>
2025-04-06 20:18:24 +0300

    add /addquiz and /quiz/save to QuizzlerController

--- src/main/java/fi/haagahelia/quizzler/web/QuizzlerController.java
+++ src/main/java/fi/haagahelia/quizzler/web/QuizzlerController.java
@@ -3,19 +3,40 @@ package fi.haagahelia.quizzler.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import fi.haagahelia.quizzler.domain.Quiz;
 import fi.haagahelia.quizzler.domain.QuizRepository;
 
 @Controller
 public class QuizzlerController {
 
-    @Autowired QuizRepository quizRepository;
+    @Autowired
+    QuizRepository quizRepository;
 
-    @RequestMapping(value = {"/quizlist"})
+    @RequestMapping(value = { "/quizlist" })
     public String quizList(Model model) {
         model.addAttribute("quizzes", quizRepository.findAll());
         return "quizlist";
+    }
+
+    @GetMapping("/addquiz")
+    public String showAddQuizForm(Model model) {
+        model.addAttribute("quiz", new Quiz());
+        return "addquiz";
+    }
+
+    @RequestMapping(value = "/quiz/save", method = RequestMethod.POST)
+    public String saveQuiz(@ModelAttribute Quiz quiz) {
+        quiz.setCreationDate(java.time.LocalDate.now());
+        if (quiz.getPublishedStatus() == null) {
+            quiz.setPublishedStatus(0);
+        }
+        quizRepository.save(quiz);
+        return "redirect:/quizlist";
     }
 
 }
