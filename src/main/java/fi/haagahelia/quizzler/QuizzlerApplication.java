262aed3b3d18675a19bb8578a71c0cd54c8f2066
MantMar <mantchaam@gmail.com>
2025-04-07 11:06:23 +0300

    Added some demo data on startup

--- src/main/java/fi/haagahelia/quizzler/QuizzlerApplication.java
+++ src/main/java/fi/haagahelia/quizzler/QuizzlerApplication.java
@@ -1,13 +1,35 @@
 package fi.haagahelia.quizzler;
 
+
+import java.time.LocalDate;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import fi.haagahelia.quizzler.domain.Quiz;
+import fi.haagahelia.quizzler.domain.QuizRepository;
 
 @SpringBootApplication
 public class QuizzlerApplication {
+	private static final Logger log = LoggerFactory.getLogger(QuizzlerApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(QuizzlerApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner quizDemo(QuizRepository quizRepo){
+		return args -> {
+			log.info("Adding some demo data");
+			quizRepo.save(new Quiz("first quiz", "test", "ABC123", 1,LocalDate.of(2025, 4, 7)));
+			quizRepo.save(new Quiz("second quiz", "test", "ABC567", 1,LocalDate.of(2025, 4, 7)));
+			quizRepo.save(new Quiz("third quiz", "test", "ABC8910", 1,LocalDate.of(2025, 4, 7)));
+			quizRepo.save(new Quiz("fourth quiz", "test", "ABC111213", 1,LocalDate.of(2025, 4, 7)));
+		};
 	}
 
 }
