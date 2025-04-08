45e22705f03969f256af42316f311a8681ba6805
nooralaakso <bhm543@myy.haaga-helia.fi>
2025-04-08 19:01:20 +0300

    update AnswersRepository.java

--- src/main/java/fi/haagahelia/quizzler/domain/AnswersRepository.java
+++ src/main/java/fi/haagahelia/quizzler/domain/AnswersRepository.java
@@ -1,9 +1,12 @@
 package fi.haagahelia.quizzler.domain;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AnswersRepository extends JpaRepository<Answers, Long> {
-
+    Answers findByQuestionQuestionId(Long questionId);
 
 }
 
