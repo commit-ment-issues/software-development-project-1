dda8ef6cce107969da75e4260afa536ba800c331
MarkusMant <mantchaam@gmail.com>
2025-04-15 15:42:20 +0300

    Early front end development

--- Quizzer/src/App.jsx
+++ Quizzer/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react';
+import { AgGridReact } from 'ag-grid-react';
 import './App.css'
 
 function App() {
@@ -6,9 +7,27 @@ function App() {
     console.log(quizzes)
   );
   
+  const [quizzes, setQuizzes] = useState([]);
+
+  useEffect(() => {
+    handleFetch();
+  }, []);
+
+  const handleFetch = () =>{
+    return fetch("http://localhost:8080/api/quizzes")
+      .then(response => {
+        if(!response.ok)
+          throw new Error("Error in fetch: " + response.statusText);
+        return response.json();
+      }).then(data => setQuizzes(data))
+  }
 
   return (
     <>
+    <h1>Quizzes</h1>
+    <div>
+      <ul>{quizzes.map((quiz) => (<li key = {quiz.quizId}>{quiz.name}</li>))}</ul>
+    </div>
     </>
   )
 }
