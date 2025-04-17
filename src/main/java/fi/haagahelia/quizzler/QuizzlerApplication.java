653d5b3c5a0956a4b4115374f4712e9323eda13e
MeeriKlemola <bhi057@myy.haaga-helia.fi>
2025-04-17 14:23:26 +0300

    fix redundant import deleted

--- src/main/java/fi/haagahelia/quizzler/QuizzlerApplication.java
+++ src/main/java/fi/haagahelia/quizzler/QuizzlerApplication.java
@@ -2,7 +2,6 @@ package fi.haagahelia.quizzler;
 
 import java.time.LocalDate;
 
-import org.aspectj.bridge.Message;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -31,37 +30,37 @@ public class QuizzlerApplication {
 		return args -> {
 			if (quizRepo.count() == 0) {
 				quizRepo.save(new Quiz("first", "test", "ABC", 0, LocalDate.of(2025, 16, 4)));
-				
+
 				log.info("Adding some demo data");
 				Quiz quiz1 = new Quiz("first quiz", "test", "ABC123", 1, LocalDate.of(2025, 4, 7));
 				Quiz quiz2 = new Quiz("second quiz", "test", "ABC567", 1, LocalDate.of(2025, 4, 7));
 				Quiz quiz3 = new Quiz("third quiz", "test", "ABC890", 0, LocalDate.of(2025, 4, 7));
 				Quiz quiz4 = new Quiz("fourth quiz", "test", "ABC123", 1, LocalDate.of(2025, 4, 7));
-				
-			quizRepo.save(quiz1);
-			quizRepo.save(quiz2);
-			quizRepo.save(quiz3);
-			quizRepo.save(quiz4);
 
-			Question question1 = new Question("What is Java?", "easy");
-			question1.setQuiz(quiz1);
-			Question question2 = new Question("What is Spring Boot?", "medium");
-			question2.setQuiz(quiz1);
-			Question question3 = new Question("Explain Dependency Injection", "hard");
-			question3.setQuiz(quiz1);
-			
-			questionRepo.save(question1);
-			questionRepo.save(question2);
-			questionRepo.save(question3);
-			
-			Answers answer1 = new Answers("A Coding language", 1);
-			answer1.setQuestion(question1);
-			Answers answer2 = new Answers("Coffee", 0);
-			answer2.setQuestion(question1);
-			
-			answersRepo.save(answer1);
-			answersRepo.save(answer2);
-		}
+				quizRepo.save(quiz1);
+				quizRepo.save(quiz2);
+				quizRepo.save(quiz3);
+				quizRepo.save(quiz4);
+
+				Question question1 = new Question("What is Java?", "easy");
+				question1.setQuiz(quiz1);
+				Question question2 = new Question("What is Spring Boot?", "medium");
+				question2.setQuiz(quiz1);
+				Question question3 = new Question("Explain Dependency Injection", "hard");
+				question3.setQuiz(quiz1);
+
+				questionRepo.save(question1);
+				questionRepo.save(question2);
+				questionRepo.save(question3);
+
+				Answers answer1 = new Answers("A Coding language", 1);
+				answer1.setQuestion(question1);
+				Answers answer2 = new Answers("Coffee", 0);
+				answer2.setQuestion(question1);
+
+				answersRepo.save(answer1);
+				answersRepo.save(answer2);
+			}
 
 		};
 	}
