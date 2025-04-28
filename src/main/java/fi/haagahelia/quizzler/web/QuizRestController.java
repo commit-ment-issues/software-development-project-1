d265c6f10b68d8827c7959ba8d3d3ad5888607be
Kaislakudjoi <159912977+Kaislakudjoi@users.noreply.github.com>
2025-04-28 15:17:44 +0300

    Add REST endpoints for getting all quizzes and quiz by ID

--- src/main/java/fi/haagahelia/quizzler/web/QuizRestController.java
+++ src/main/java/fi/haagahelia/quizzler/web/QuizRestController.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import fi.haagahelia.quizzler.domain.QuizRepository;
 import fi.haagahelia.quizzler.domain.Quiz;
@@ -19,8 +22,19 @@ public class QuizRestController {
     @Autowired
     private QuizRepository quizRepository;
 
+    @GetMapping("/quizzes/{id}")
+    public Quiz getQuizById(@PathVariable Long id) {
+        return quizRepository.findById(id).orElseThrow(
+            () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Quiz with the provided id " + id + " does not exist"));
+    }
+
     @GetMapping("/quizzes")
     public List<Quiz> getAllQuizzes() {
+        return quizRepository.findAll();
+    }
+
+    @GetMapping("/quizzes/published")
+    public List<Quiz> getPublishedQuizzes() {
         return quizRepository.findAllByPublishedStatus(1);
     }
 }
