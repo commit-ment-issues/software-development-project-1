c4e3345582145c805d08c552e572da2a8586d5ce
MeeriKlemola <bhi057@myy.haaga-helia.fi>
2025-04-28 23:30:31 +0300

    add endpoints to all categories and singular category

--- src/main/java/fi/haagahelia/quizzler/web/CategoryRestController.java
+++ src/main/java/fi/haagahelia/quizzler/web/CategoryRestController.java
@@ -1,11 +1,18 @@
 package fi.haagahelia.quizzler.web;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import fi.haagahelia.quizzler.domain.CategoryRepository;
+import fi.haagahelia.quizzler.domain.Category;
 
 @RestController
 @RequestMapping("/api")
@@ -14,5 +21,17 @@ import fi.haagahelia.quizzler.domain.CategoryRepository;
 public class CategoryRestController {
     @Autowired
     private CategoryRepository categoryRepository;
+
+    @GetMapping("/categories/{id}")
+    public Category getCategoryById(@PathVariable Long id) {
+        return categoryRepository.findById(id).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+                        "Category with the provided id " + id + " does not exist"));
+    }
+
+    @GetMapping("/categories")
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
 
 }
