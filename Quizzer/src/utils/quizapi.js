2669bde0f9178c571c1b752e26fbf01a68e2ee4f
Kaislakudjoi <159912977+Kaislakudjoi@users.noreply.github.com>
2025-04-28 15:29:33 +0300

    fix getQuizzes fetch in quizapi

--- Quizzer/src/utils/quizapi.js
+++ Quizzer/src/utils/quizapi.js
@@ -1,8 +1,14 @@
 export function getQuizzes() {
-    return fetch("http://localhost:8080/api/quizzes")
-      .then(response => {
-        if(!response.ok)
-          throw new Error("Error in fetch: " + response.statusText);
-        return response.json();
-        });
+  return fetch("http://localhost:8080/api/quizzes", {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
     }
+  })
+    .then(response => {
+      if (!response.ok)
+        throw new Error("Error in fetch: " + response.statusText);
+      return response.json();
+    });
+}
