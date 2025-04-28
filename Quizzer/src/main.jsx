958f03722d41b83168cfc4253667a91d5fbefb83
MeeriKlemola <bhi057@myy.haaga-helia.fi>
2025-04-28 23:29:07 +0300

    add routes to categorylist and to categorypage

--- Quizzer/src/main.jsx
+++ Quizzer/src/main.jsx
@@ -2,13 +2,17 @@ import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { HashRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
+import CategoryList from './Components/CategoryList.jsx'
+import CategoryPage from './Components/CategoryPage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
-      
+        <Route path="categories" element={<CategoryList />} />
+        <Route path="categories/:categoryid" element={<CategoryPage />} />
+        
       </Routes>
     </HashRouter>
 
