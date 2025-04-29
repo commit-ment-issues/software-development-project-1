101a41d2ed072f70675ba6e2aefa29613c3300d1
nooralaakso <bhm543@myy.haaga-helia.fi>
2025-04-29 14:33:30 +0300

    update main.jsx

--- Quizzer/src/main.jsx
+++ Quizzer/src/main.jsx
@@ -4,12 +4,14 @@ import { HashRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
 import CategoryList from './Components/CategoryList.jsx'
 import CategoryPage from './Components/CategoryPage.jsx'
+import QuestionList from './Components/QuestionList.jsx'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path='quiz/:quizId' element={<QuestionList />} />
         <Route path="categories" element={<CategoryList />} />
         <Route path="categories/:categoryid" element={<CategoryPage />} />
         
