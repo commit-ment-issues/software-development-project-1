361d44bb01f1e0da8ee7305048949b51199eb86b
Kaislakudjoi <159912977+Kaislakudjoi@users.noreply.github.com>
2025-05-03 19:15:56 +0300

    add REST API endpoint for creating an answer for a question

--- src/main/java/fi/haagahelia/quizzler/domain/AnswersRepository.java
+++ src/main/java/fi/haagahelia/quizzler/domain/AnswersRepository.java
@@ -2,6 +2,7 @@ package fi.haagahelia.quizzler.domain;
 
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface AnswersRepository extends JpaRepository<Answers, Long> {
     List<Answers> findByQuestionQuestionId(Long questionId);
+    Optional<Answers> findById(Long Id);
 
 }
 
--- src/main/java/fi/haagahelia/quizzler/web/AnswerRestController.java
+++ src/main/java/fi/haagahelia/quizzler/web/AnswerRestController.java
@@ -5,6 +5,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -12,22 +14,45 @@ import java.util.List;
 
 import fi.haagahelia.quizzler.domain.Answers;
 import fi.haagahelia.quizzler.domain.AnswersRepository;
+import fi.haagahelia.quizzler.domain.CreateAnswerDTO;
+import fi.haagahelia.quizzler.domain.Question;
 
 @RestController
 @RequestMapping("/api")
 @CrossOrigin(origins = "*")
-public class AnswerRestController {   
+public class AnswerRestController {
     @Autowired
     private AnswersRepository answerRepository;
 
     @GetMapping("/question/{id}/answers")
     public List<Answers> getAnswersByQuestionId(@PathVariable Long id) {
         List<Answers> answers = answerRepository.findByQuestionQuestionId(id);
-        
+
         if (answers.isEmpty()) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Question with the provided id " + id + " does not exist");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+                    "Question with the provided id " + id + " does not exist");
         }
         return answers;
+    }
+
+    @PostMapping("/answers")
+    public void CreateAnswer(@RequestBody CreateAnswerDTO dto) {
+        Long optionId = dto.getAnswerOptionId();
+        
+        if (dto.getAnswerOptionId() == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Answer option ID is not provided.");
+        }
+
+        Answers selectedAnswerOption = answerRepository.findById(dto.getAnswerOptionId())
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Answer option with the provided ID does not exist."));
+
+        Question question = selectedAnswerOption.getQuestion();
+        if (question == null || question.getQuiz() == null || question.getQuiz().getPublishedStatus() == null
+                || question.getQuiz().getPublishedStatus() != 1) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Quiz is not published.");
+        }
+
+        answerRepository.save(selectedAnswerOption); 
     }
 
 }
