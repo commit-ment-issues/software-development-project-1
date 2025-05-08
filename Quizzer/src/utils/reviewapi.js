151c12b1ca0098ad3f681c1a72f93a5312d2692a
nooralaakso <bhm543@myy.haaga-helia.fi>
2025-05-08 15:13:42 +0300

    add getReviewsByQuizId api fetch in reviewapi.js

--- Quizzer/src/utils/reviewapi.js
+++ Quizzer/src/utils/reviewapi.js
@@ -13,18 +13,28 @@ export async function getReviews() {
     });
 }
 
-export async function addReview(){
-    return fetch('http://localhost:8080/api/quizzes/${quizId}/reviews', {
-        method: "POST",
-        headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json"
-        }
-    })
-        .then(response => {
-        if (!response.ok)
-            throw new Error("Error in fetch: " + response.statusText);
-        return response.json();
-        });
-    
-}
+export async function addReview() {
+  return fetch('http://localhost:8080/api/quizzes/${quizId}/reviews', {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    }
+  })
+    .then(response => {
+      if (!response.ok)
+        throw new Error("Error in fetch: " + response.statusText);
+      return response.json();
+    });
+
+
+  }
+
+  export async function getReviewsByQuizId(id) {
+    const response = await fetch(`http://localhost:8080/api/reviews/quiz/${id}`);
+    if (!response.ok) {
+      throw new Error("Error in fetch: " + response.statusText);
+    }
+    return await response.json();
+  }
+
